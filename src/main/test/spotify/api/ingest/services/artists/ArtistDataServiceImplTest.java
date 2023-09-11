679ab738805e905da7b8066e3921129c0473f654
bmg13 <gabcmonteiro@gmail.com>
2023-09-11 12:41:45 +0100

    Add UT structure

---
+++ src/main/test/spotify/api/ingest/services/artists/ArtistDataServiceImplTest.java
@@ -0,0 +1,5 @@
+package spotify.api.ingest.services.users.artists;
+
+public class ArtistDataServiceImplTest {
+
+}
---
+++ src/main/test/spotify/api/ingest/services/users/UsersDataServiceImplTest.java
@@ -0,0 +1,5 @@
+package spotify.api.ingest.services.users;
+
+public class UsersDataServiceImplTest {
+
+}
